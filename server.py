41183d26e9c8b7d3c71a5fe63e0471ce9f3753ba
Vasilii Safronov <vasi1988@mail.ru>
2021-05-30 22:04:41 +0300

    added html template

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
 
 app = Flask(__name__)
@@ -6,11 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    title = "Прогноз погоды"
     weather = weather_by_city("Moscow,Russia")
-    if weather:
-        return f"Погода сегодня {weather['temp_C']} градусов, ощущается как {weather['FeelsLikeC']}"
-    else:
-        return "Сервис погоды временно недоступен"
+    return render_template("index.html", page_title=title, weather=weather)
 
 
 if __name__ == "__main__":
